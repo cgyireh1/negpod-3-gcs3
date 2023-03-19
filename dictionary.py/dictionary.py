3c92c1b421f64d8f662864a0d8c098448145636e
root <g.warsameh@alustudent.com>
2023-03-19 12:59:45 -0700

    dictionary.py

--- dictionary.py/dictionary.py
+++ dictionary.py/dictionary.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-def roman_to_int(s):
+def roman_to_int(roman_numeral):
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = 0
-    prev_val = 0
-    for i in range(len(s)-1, -1, -1):
-        if roman_dict[s[i]] < prev_val:
-            num -= roman_dict[s[i]]
+    total = 0
+    prev_value = 0
+    for i in range(len(roman_numeral) - 1, -1, -1):
+        current_value = roman_dict[roman_numeral[i]]
+        if current_value < prev_value:
+            total -= current_value
         else:
-            num += roman_dict[s[i]]
-        prev_val = roman_dict[s[i]]
-    return num
+            total += current_value
+        prev_value = current_value
+    return total
