c5ca2c0559f7caa7afda515f0451f7ba25426881
root <g.warsameh@alustudent.com>
2023-03-19 13:06:34 -0700

    dictionary.py

--- dictionary.py/dictionary.py
+++ dictionary.py/dictionary.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-def roman_to_int(roman_numeral):
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    prev_value = 0
-    for i in range(len(roman_numeral) - 1, -1, -1):
-        current_value = roman_dict[roman_numeral[i]]
-        if current_value < prev_value:
-            total -= current_value
+def roman_to_int(s: str) -> int:
+    roman_map = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+    result = 0
+    i = 0
+    while i < len(s):
+        if i+1 < len(s) and roman_map[s[i]] < roman_map[s[i+1]]:
+            result += roman_map[s[i+1]] - roman_map[s[i]]
+            i += 2
         else:
-            total += current_value
-        prev_value = current_value
-    return total
+            result += roman_map[s[i]]
+            i += 1
+    return result
